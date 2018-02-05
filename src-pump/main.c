13fc697117d1478652ab2cab6d3be40d2231da65
Muhammad Salman <muhammads09@hotmail.com>
2018-02-05 16:37:59 -0500

    Removed unnecessary motor control

--- src-pump/main.c
+++ src-pump/main.c
@@ -7,19 +7,9 @@
 #define OUTPUT_PIN_B_1 15
 #define OUTPUT_PIN_A_1 18
 
-#define OUTPUT_PIN_E_2 17
-#define OUTPUT_PIN_B_2 27
-#define OUTPUT_PIN_A_2 22
-
 
 
 #define MICRO_SEC_IN_SEC 1000000
-
-typedef struct time_tracker {
-	long int start;
-	long int end;
-	long int current_distance;
-} time_tracker;
 
 int main() {
 	
@@ -30,18 +20,9 @@ int main() {
 	gpioSetMode(OUTPUT_PIN_E_1, PI_OUTPUT);
 	gpioSetMode(OUTPUT_PIN_B_1, PI_OUTPUT);
 	gpioSetMode(OUTPUT_PIN_A_1, PI_OUTPUT);
-	
-	gpioSetMode(OUTPUT_PIN_E_2, PI_OUTPUT);
-	gpioSetMode(OUTPUT_PIN_B_2, PI_OUTPUT);
-	gpioSetMode(OUTPUT_PIN_A_2, PI_OUTPUT);
-
 	gpioWrite(OUTPUT_PIN_E_1, 1);
 	gpioWrite(OUTPUT_PIN_B_1, 0);
 	gpioWrite(OUTPUT_PIN_A_1, 1);
-
-	gpioWrite(OUTPUT_PIN_E_2, 1);
-	gpioWrite(OUTPUT_PIN_B_2, 0);
-	gpioWrite(OUTPUT_PIN_A_2, 1);
 
 	exit(0);
 }
