9a3836149979cc5fbc9b80c332e0b4315ad64f84
zohaibmq <zohaibmq@gmail.com>
2018-02-07 18:36:20 -0500

    Added basic drive motor functions

--- src-motor/main.c
+++ src-motor/main.c
@@ -3,15 +3,15 @@
 #include<stdlib.h>
 #include <sys/types.h>
 
+//this is the left motor
 #define OUTPUT_PIN_E_1 14
 #define OUTPUT_PIN_B_1 15
 #define OUTPUT_PIN_A_1 18
 
+//this is the right motor
 #define OUTPUT_PIN_E_2 17
 #define OUTPUT_PIN_B_2 27
 #define OUTPUT_PIN_A_2 22
-
-
 
 #define MICRO_SEC_IN_SEC 1000000
 
@@ -20,6 +20,112 @@ typedef struct time_tracker {
 	long int end;
 	long int current_distance;
 } time_tracker;
+
+bool MotorsInit(){ //returns 1 if failure to initialize
+	
+	if (gpioInitialise() < 0) {
+		fprintf(stderr, "Failed to initialize GPIO interface");
+		return 1;
+	}
+	gpioSetMode(OUTPUT_PIN_E_1, PI_OUTPUT);
+	gpioSetMode(OUTPUT_PIN_B_1, PI_OUTPUT);
+	gpioSetMode(OUTPUT_PIN_A_1, PI_OUTPUT);
+	
+	gpioSetMode(OUTPUT_PIN_E_2, PI_OUTPUT);
+	gpioSetMode(OUTPUT_PIN_B_2, PI_OUTPUT);
+	gpioSetMode(OUTPUT_PIN_A_2, PI_OUTPUT);
+	
+	gpioWrite(OUTPUT_PIN_E_1, 0);
+	gpioWrite(OUTPUT_PIN_E_2, 0);
+	
+	return 0;
+	
+}
+
+void SlowLeft(){
+	
+	gpioWrite(OUTPUT_PIN_E_1, 1);
+	gpioWrite(OUTPUT_PIN_E_2, 1);
+	
+	//turn right motor forwards
+	gpioWrite(OUTPUT_PIN_B_2, 0);
+	gpioWrite(OUTPUT_PIN_A_2, 1);
+		
+}
+
+void SlowRight(){
+	
+	gpioWrite(OUTPUT_PIN_E_1, 1);
+	gpioWrite(OUTPUT_PIN_E_2, 1);
+	
+	//turn left motor forwards
+	gpioWrite(OUTPUT_PIN_B_1, 0);
+	gpioWrite(OUTPUT_PIN_A_1, 1);
+		
+}
+
+void SharpLeft(){
+	
+	gpioWrite(OUTPUT_PIN_E_1, 1);
+	gpioWrite(OUTPUT_PIN_E_2, 1);
+	
+	//turn left motor backwards
+	gpioWrite(OUTPUT_PIN_B_1, 1);
+	gpioWrite(OUTPUT_PIN_A_1, 0);
+	
+	//turn right motor forwards
+	gpioWrite(OUTPUT_PIN_B_2, 0);
+	gpioWrite(OUTPUT_PIN_A_2, 1);
+		
+}
+
+void SharpRight(){
+	
+	gpioWrite(OUTPUT_PIN_E_1, 1);
+	gpioWrite(OUTPUT_PIN_E_2, 1);
+	
+	//turn left motor forwards
+	gpioWrite(OUTPUT_PIN_B_1, 0);
+	gpioWrite(OUTPUT_PIN_A_1, 1);
+	
+	//turn right motor backwards
+	gpioWrite(OUTPUT_PIN_B_2, 1);
+	gpioWrite(OUTPUT_PIN_A_2, 0);
+		
+}
+
+void DriveStraight(){
+	
+	gpioWrite(OUTPUT_PIN_E_1, 1);
+	gpioWrite(OUTPUT_PIN_E_2, 1);
+	
+	gpioWrite(OUTPUT_PIN_B_1, 0);
+	gpioWrite(OUTPUT_PIN_A_1, 1);
+	
+	gpioWrite(OUTPUT_PIN_B_2, 0);
+	gpioWrite(OUTPUT_PIN_A_2, 1);
+
+}
+
+void DriveBack(){
+	
+	gpioWrite(OUTPUT_PIN_E_1, 1);
+	gpioWrite(OUTPUT_PIN_E_2, 1);
+	
+	gpioWrite(OUTPUT_PIN_B_1, 1);
+	gpioWrite(OUTPUT_PIN_A_1, 0);
+	
+	gpioWrite(OUTPUT_PIN_B_2, 1);
+	gpioWrite(OUTPUT_PIN_A_2, 0);
+
+}
+
+void StopMotors(){
+	
+	gpioWrite(OUTPUT_PIN_E_1, 0);
+	gpioWrite(OUTPUT_PIN_E_2, 0);
+
+}
 
 int main() {
 	if (gpioInitialise() < 0) {
